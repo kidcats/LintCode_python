09f7b51f27333b564ce26b9b4bf61d241277c731
kidcat <1453663017@qq.com>
2017-01-11 11:00:14 +0800

    2017.1

--- ceshi.py
+++ ceshi.py
@@ -1,22 +1,40 @@
-def binarySearch(A,k):
-    # write your code here
-    index=0
-    for i in range(k):
-        index=deleteBig(A)
-        A=A.replace(A[index],'',1)
-        if A.startswith('0'):
-            A=A[1:]
-    return A
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
 
-def deleteBig(A):
-    for i in range(len(A)-1):
-        if A[i+1]<A[i]:
-            return i
-    return len(A)-1
-
-        
+'''
+递归的思路是求两个数满足
+'''
+def rotateRight(head, k):
+        # write your code here
+        #指针走到倒数第K个位置处,形成环然后打断
+        if head==None or head.next==None:
+            return head
+        pre=head
+        end=head
+        curr=head
+        j=0
+        i=0
+        while end.next!=None:
+            end=end.next
+            j+=1
+        kk=k%(j+1)
+        end=head
+        if kk==0:
+            return pre 
+        while end.next!=None:
+            if i>=kk-1:
+                curr=pre
+                pre=pre.next
+            end=end.next
+            i+=1
+        end.next=head
+        curr.next=None
+        return pre
 
 
 if __name__=='__main__':
-    a=binarySearch('90249',1)
-    print(a)
+    l=ListNode(1,ListNode(2,ListNode(3,ListNode(2,ListNode(1)))))
+    a=rotateRight(l,10)
+    print(a.val)
